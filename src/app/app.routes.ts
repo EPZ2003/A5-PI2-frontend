0f094f377d6487b108851de60f5e2e35797723a4
Polizzi Enzo <enzo.polizzi.11@gmail.com>
2025-11-17 11:35:54 +0100

    V1Page-Be_able_to_enter_data

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+<<<<<<< HEAD
 import HomepageComponent from './homepage/homepage.component';
 
 export const routes: Routes = [
@@ -6,5 +7,13 @@ export const routes: Routes = [
 		path:'',
 		component:HomepageComponent
 
+=======
+import {V1GazEmissionComponent} from './v1GazEmission/v1GazEmission.component';
+
+export const routes: Routes = [
+	{
+		path:'v1',
+		component:V1GazEmissionComponent
+>>>>>>> 8cdc8e5 (V1Page-Be_able_to_enter_data)
 	}
 ];
---
+++ src/app/v1GazEmission/v1GazEmission.component.ts
@@ -0,0 +1,12 @@
+import {Component} from "@angular/core";
+
+@Component({
+	selector:'v1GazEmission',
+	templateUrl: 'v1GazEmission.component.html',
+	imports: [
+		
+	]
+})
+export class V1GazEmissionComponent {
+	
+}
