1ab4bd70c191d0aa0d3c43e16b411340aa9a9826
Polizzi Enzo <enzo.polizzi.11@gmail.com>
2025-12-08 15:08:00 +0100

    V4Page-Can_use_Criteria_1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import HomepageComponent from './homepage/homepage.component';
 import {V1GazEmissionComponent} from './v1GazEmission/v1GazEmission.component';
+import {V4HealthSecurityWorkConditionComponent} from './v4HealthSecurityConditionWork/v4HealthSecurityWorkCondition.component';
 
 export const routes: Routes = [
 	{
@@ -10,5 +11,9 @@ export const routes: Routes = [
 	{
 		path:'v1',
 		component:V1GazEmissionComponent
+	},
+	{
+		path: 'v4',
+		component: V4HealthSecurityWorkConditionComponent
 	}
 ];
---
+++ src/app/shared/v4HealthSecurityWorkCondition.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from "@angular/core";
+import {AppConstants} from "../app.constant";
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+	providedIn:'root'
+})
+export class V4HealthSecurityWorkCondtionService{
+	private url = AppConstants.PRIVATE_URL+'v4'
+	constructor(
+		private httClient:HttpClient
+	){}
+
+	save(fmData:any){
+		return this.httClient.post(this.url, fmData)
+	}
+}
